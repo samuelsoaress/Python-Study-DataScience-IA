533ad79bc384f7a146fbad97dc4e58d68f5a1717
getsam <samuelsoares177778@gmail.com>
2019-11-26 14:09:53 -0300

    add dir basico

---
+++ basico/script-adm.py
@@ -0,0 +1,7 @@
+import json
+with open('input.json','r') as input:
+    obj = json.load(input)
+    with open('hobbie.txt','w') as output:
+        output.write(obj['name'] + "'s Hobbies:/n")
+        for hobby in obj['hobbies']:
+            output.write(hobby + "/n")
