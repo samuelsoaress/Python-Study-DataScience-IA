b6556883110f6b05534b6e0ec0157b8545c9a842
getsam <samuelsoares177778@gmail.com>
2020-01-27 16:49:26 -0300

    comments

--- Estatistica II Python/regressao_multipla.py
+++ Estatistica II Python/regressao_multipla.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-import statsmodels.formula.api as sm
+import statsmodels.formula.api as sm # lib para fazer calculos estatisticos que ja vem por padrão no R 
 
 base = pd.read_csv('mt_cars.csv')
 base = base.drop(['Unnamed: 0'], axis = 1)
 
-X = base.iloc[:, 2].values
-y = base.iloc[:, 0].values
+X = base.iloc[:, 2].values #variavel independente
+y = base.iloc[:, 0].values # variavel dependente
 correlacao = np.corrcoef(X, y)
 X = X.reshape(-1, 1)
 
@@ -16,10 +16,10 @@ modelo = LinearRegression()
 modelo.fit(X, y)
 modelo.intercept_
 modelo.coef_
-modelo.score(X, y)
+modelo.score(X, y) #score vai mostrar quanto as colunas independentes explicam a dependentes
 
 previsoes = modelo.predict(X)
-modelo_ajustado = sm.ols(formula = 'mpg ~ disp', data = base)
+modelo_ajustado = sm.ols(formula = 'mpg ~ disp', data = base) # ajustando modelo
 modelo_treinado = modelo_ajustado.fit()
 modelo_treinado.summary()
 
@@ -28,17 +28,17 @@ plt.plot(X, previsoes, color = 'red')
 
 modelo.predict(200)
 
-X1 = base.iloc[:, 1:4].values
-y1 = base.iloc[:, 0].values
+X1 = base.iloc[:, 1:4].values # Pegando aqui mais baias
+y1 = base.iloc[:, 0].values # pegando a dependente
 modelo2 = LinearRegression()
-modelo2.fit(X1, y1)
+modelo2.fit(X1, y1) 
 
 modelo2.score(X1, y1)
 modelo_ajustado2 = sm.ols(formula = 'mpg ~ cyl + disp + hp', data = base)
 modelo_treinado2 = modelo_ajustado2.fit()
 modelo_treinado2.summary()
 
-novo = np.array([4, 200, 100])
-novo = novo.reshape(1, -1)
+novo = np.array([4, 200, 100]) # novo array para passar para o predict na sequecia do modelo
+novo = novo.reshape(1, -1) # coloca os atributos em uma coluna
 modelo2.predict(novo)
 
