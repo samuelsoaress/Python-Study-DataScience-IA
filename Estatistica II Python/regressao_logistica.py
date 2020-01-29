eeeb221c02c73cca163c4fe068d81ce6586419ce
getsam <samuelsoares177778@gmail.com>
2020-01-29 12:17:01 -0300

    comentando partes confusas

--- Estatistica II Python/regressao_logistica.py
+++ Estatistica II Python/regressao_logistica.py
@@ -7,23 +7,24 @@ base = pd.read_csv('Eleicao.csv', sep = ';')
 plt.scatter(base.DESPESAS, base.SITUACAO)
 base.describe()
 
-np.corrcoef(base.DESPESAS, base.SITUACAO)
+np.corrcoef(base.DESPESAS, base.SITUACAO) #verificando correlação das duas variaveis
 
 X = base.iloc[:, 2].values
 X = X[:, np.newaxis]
 y = base.iloc[:, 1].values
 
 modelo = LogisticRegression()
-modelo.fit(X, y)
+modelo.fit(X, y) #Treinando modelo
 modelo.coef_
 modelo.intercept_
 
 plt.scatter(X, y)
-X_teste = np.linspace(10, 3000, 100)
+X_teste = np.linspace(10, 3000, 100) # gerando numeros aleatorios
+
 def model(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x)) # retorna sigmoid 
 r = model(X_teste * modelo.coef_ + modelo.intercept_).ravel()
-plt.plot(X_teste, r, color = 'red')
+plt.plot(X_teste, r, color = 'red')  # plota o mesmo grafico feito em r mostrando a curva da diferença entre os candidatos que gastaram mais
 
 base_previsoes = pd.read_csv('NovosCandidatos.csv', sep = ';')
 despesas = base_previsoes.iloc[:, 1].values
